beed6b58784731358f2b41555a42bdea9a48eb88
yactouat <yacine.touati.pro@gmail.com>
2024-05-09 23:51:57 +0200

    can we run this thing in the background now? take #3

--- stop-app.sh
+++ stop-app.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# check if we have more at least two `python3 -m streamlit run streamlit_app.py` processes running
-# shellcheck disable=SC2046
-if [ $(ps aux | grep 'python3 -m streamlit run streamlit_app.py' | wc -l) -gt 1 ]; then
-    # kill all `python3 -m streamlit run streamlit_app.py` processes
-    pkill -f 'python3 -m streamlit run streamlit_app.py'
+# find the PID of the processes having 'streamlit_app.py' in their name
+# we use 'pgrep' with '-f' to match against the full process command line
+
+pids=$(pgrep -f "streamlit_app.py")
+
+# check if any PID was found
+if [ ! -z "$pids" ]; then
+    # loop through each PID and kill the process
+    for pid in $pids; do
+        kill $pid
+        # check if kill command was successful
+        if [ $? -eq 0 ]; then
+            echo "Process with PID $pid has been stopped."
+        else
+            echo "Failed to stop process with PID $pid."
+        fi
+    done
+else
+    echo "No matching processes found."
 fi
